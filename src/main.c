22fc0db5af64b4c668a153a138c4fde1133bfe20
Quinn Caverly <quinncaverly@gmail.com>
2024-10-26 12:19:13 -0400

    testing taking input dir as cli arg

--- src/main.c
+++ src/main.c
@@ -4,9 +4,29 @@
 #include "../include/math/math_processor.h"
 #include "../include/ml/ml_processor.h"
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-  InputData data = import_csv("../../input");
+int main(int argc, char *argv[]) {
+  const char *input_dir = NULL;
+  bool use_ml = false; // Default to math processing
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--ml") == 0) {
+      use_ml = true;
+    } else if (input_dir == NULL) {
+      input_dir = argv[i];
+    } else {
+      printf("Error: Unexpected argument '%s'\n", argv[i]);
+      return 1;
+    }
+  }
+
+  if (input_dir == NULL) {
+    printf("Error: No input directory specified\n");
+    return 1;
+  }
+
+  InputData data = import_csv(input_dir);
 
   printf("Random queries for testing:\n");
   printf("Is employee_available: %d\n", is_employee_available(&data, 0, 0, 0));
@@ -15,7 +35,6 @@ int main() {
 
   const DataProcessor *processor;
 
-  const int use_ml = 1;
   // Conditional logic to decide between ML and Math paths
   if (use_ml) {
     processor = &ML_PROCESSOR;
